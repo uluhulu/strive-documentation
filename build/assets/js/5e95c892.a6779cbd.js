54fbec740b27ea273c0b1d73a7e065d0c0afd4a6
uluhulu <ulianasisoeva@gmail.com>
2025-04-07 16:19:39 +0500

    add build

---
+++ build/assets/js/5e95c892.a6779cbd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkstrive_documentation=self.webpackChunkstrive_documentation||[]).push([[647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>o});r(6540);var t=r(4164),n=r(1769),u=r(204),a=r(2831),c=r(1410),i=r(4848);function o(e){return(0,i.jsx)(n.e3,{className:(0,t.A)(u.G.wrapper.docsPages),children:(0,i.jsx)(c.A,{children:(0,a.v)(e.route.routes)})})}}}]);
---
+++ build/assets/js/a7456010.66410085.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkstrive_documentation=self.webpackChunkstrive_documentation||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
